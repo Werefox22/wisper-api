1f200dc0a93a183b617bc9d6fa735cef708b2d29
Werefox <jack@wrightus.com>
2022-07-18 20:34:44 -0400

    Created post model and migration

---
+++ migrations/20220719003427-create-post.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('posts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      id: {
+        type: Sequelize.INTEGER
+      },
+      body: {
+        type: Sequelize.STRING
+      },
+      likes: {
+        type: Sequelize.INTEGER
+      },
+      shares: {
+        type: Sequelize.INTEGER
+      },
+      edited: {
+        type: Sequelize.BOOLEAN
+      },
+      date: {
+        type: Sequelize.DATE
+      },
+      user_id: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('posts');
+  }
+};
---
+++ models/post.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class post extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  post.init({
+    id: DataTypes.INTEGER,
+    body: DataTypes.STRING,
+    likes: DataTypes.INTEGER,
+    shares: DataTypes.INTEGER,
+    edited: DataTypes.BOOLEAN,
+    date: DataTypes.DATE,
+    user_id: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'post',
+  });
+  return post;
+};
