39805acebe87567aa5a50be0c8dcf83ab044925c
Werefox <jack@wrightus.com>
2022-07-20 19:48:05 -0400

    Commented out sequelize in the server.js

--- server.js
+++ server.js
@@ -6,35 +6,35 @@ app.use(express.json())
 // dotenv
 require('dotenv').config()
 
-// Sequelize
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize({    
-	"username": process.env.DATABASE_USER,
-	"password": process.env.DATABASE_PASS,
-	"database": process.env.DATABASE_DATA,
-	"host": process.env.DATABASE_HOST,
-	"dialect":"postgres",
-	"protocol":"postgres",
-	"port": process.env.DATABASE_PORT, 
-	"dialectOptions": {
-		"ssl": {
-			"require": true,
-			"rejectUnauthorized": false
-		}
-	}
-});
+// // Sequelize
+// const Sequelize = require('sequelize');
+// const sequelize = new Sequelize({    
+// 	"username": process.env.DATABASE_USER,
+// 	"password": process.env.DATABASE_PASS,
+// 	"database": process.env.DATABASE_DATA,
+// 	"host": process.env.DATABASE_HOST,
+// 	"dialect":"postgres",
+// 	"protocol":"postgres",
+// 	"port": process.env.DATABASE_PORT, 
+// 	"dialectOptions": {
+// 		"ssl": {
+// 			"require": true,
+// 			"rejectUnauthorized": false
+// 		}
+// 	}
+// });
 
-sequelize
-	.authenticate()
-	.then(() => {
-		console.log('Connection has been established successfully.');
-	})
-	.catch(err => {
-		console.error('Unable to connect to the database:', err);
-	});
+// sequelize
+// 	.authenticate()
+// 	.then(() => {
+// 		console.log('Connection has been established successfully.');
+// 	})
+// 	.catch(err => {
+// 		console.error('Unable to connect to the database:', err);
+// 	});
 
-const db = require('./models')
-db.sequelize.sync({ force: true })
+// const db = require('./models')
+// db.sequelize.sync({ force: true })
 
 // routes
 app.get('/', (req, res) => {
